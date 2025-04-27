e040450b904c5a3920c9c07b150d250eebf13b0e
rhita123 <rhita.moummade@efrei.net>
2025-04-27 19:04:39 +0200

    feat: configurer axios globalement pour les appels API

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,18 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
+// src/main.js
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app');
+Vue.config.productionTip = false
+
+// 🔥 Configurer Axios
+axios.defaults.baseURL = 'http://localhost:5050/api'
+
+// 🔥 Ajouter axios globalement
+Vue.prototype.$http = axios
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
