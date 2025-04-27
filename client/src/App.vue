032bd17347a8f37fc6eb2d17f828980726841cb3
rhita123 <rhita.moummade@efrei.net>
2025-04-27 21:13:45 +0200

    ajuster css

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,12 +7,7 @@
         <router-link to="/works">Films & Séries</router-link>
         <router-link to="/login">Connexion</router-link>
         <router-link to="/register">Inscription</router-link>
-        <router-link 
-            v-if="user && user.role === 'admin'" 
-              to="/admin" 
-                class="nav-link">
-            Admin
-        </router-link>
+        
       </div>
     </nav>
     
@@ -37,14 +32,13 @@ export default {
   }
 }
 </script>
-
 <style scoped>
 .navbar {
   background-color: #f25c3b;
   padding: 10px 30px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-between; /* Laisser space-between pour diviser la zone entre le logo et les liens */
   position: fixed;
   width: 100%;
   top: 0;
@@ -53,11 +47,14 @@ export default {
 
 .logo {
   height: 50px;
+  margin-right: 50px; /* Ajouter un petit espace après le logo */
 }
 
 .nav-links {
   display: flex;
   gap: 25px;
+  justify-content: center; /* Centrer les liens de navigation */
+  flex: 1; /* Prendre tout l'espace restant pour centrer les liens */
 }
 
 .nav-links a {
