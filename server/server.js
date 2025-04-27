68fc5ff8fb1d6f8b7fc089bea8ac72da62954365
rhita123 <rhita.moummade@efrei.net>
2025-04-27 17:43:12 +0200

    connexion des routes d'authentification au serveur

--- server/server.js
+++ server/server.js
@@ -1,21 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();
-
 const app = express();
-const PORT = process.env.PORT || 5000;
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-// ROUTE DE TEST
-app.get('/', (req, res) => {
-  res.json({ message: 'Backend Popcorn & Opinions prêt 🚀' });
-});
+// Routes
+app.use('/api', authRoutes);
 
-// Lancer le serveur
+// Démarrage du serveur
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
 });
